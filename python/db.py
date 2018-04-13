1599e2ca1eea48e003a08b8cd428e82c6143ef54
Charlie Yi Zhu <charliezhu@Charlies-MacBook-Pro.local>
2018-04-13 11:09:18 -0700

    Add comment. Organize files.

--- python/db.py
+++ python/db.py
@@ -1,14 +1,24 @@
-import psycopg2
+import psycopg2 # import PostgreSQL database interface library.
 import traceback
 
 import sys
-sys.path.insert(0,"/Users/abc/git/cloud/etl")
+# Add PYTHONPATH, then we may import all the objects below this folder.
+sys.path.insert(0,"/Users/abc/git/cloud/etl")  
+
+# import tool to parse .ini configuration file.
+# See below link for the explaination and examples:
+# http://www.postgresqltutorial.com/postgresql-python/connect/
+# ConfigParser().read(), returns 2 dimensional array, e.g.: [[a1,a2],[b1,b2], ...].
 from etl.pipeline_core.config import Config
 
 class DbTool(object):
-  # def __init__() # put config setter here?
-  config = Config.load("/Users/abc/aws/database.ini")
+  # initialize a local field(object property/attribute). Constructor.
+  # config = Config.load("/Users/abc/aws/database.ini")
+  def __init__(self, ini_file) # put config constructor/initializer here?
+    config = Config.load(ini_file)
 
+  # Build a dictionary object to connect to a database.
+  # {'user': 'scala_user', 'password': '', 'host': 'localhost', 'database': 'scala_db'}
   def db_config(self, db_name):
     db = {}
     params = self.config.settings.items(db_name)
@@ -21,6 +31,7 @@ class DbTool(object):
     return self.config.get('app_name_db','password')
 
 def test_query(conn, app_name_db_passwd):
+  # Use loan pattern to open a cursor, it'll be automatically closed after exit the scope.
   with conn.cursor() as cur:    
     cur.execute("""select count(*) from version() """)
     rows = cur.fetchall()
@@ -30,13 +41,17 @@ def test_query(conn, app_name_db_passwd):
 
 def main():
   try:
-      app_name_tool = DbTool()
+      app_name_tool = DbTool("/Users/charliezhu/work/blastworks/aws/database.ini")
       app_name_db = app_name_tool.db_config(db_name = 'local_db')
+      # Same loan pattern, but for database connection.
+      # **app_name_db, unpack dictionary to key value pairs arguments lists.
+      # see, https://docs.python.org/3.7/tutorial/controlflow.html#unpacking-argument-lists
       with psycopg2.connect(**app_name_db) as conn:
         test_query(conn, app_name_tool.app_name_db_passwd())
   except:
       traceback.print_exc()
       print("I am unable to connect to the database")
 
+#  if run as the single script, run main method.
 if __name__ == '__main__':
   main()
