4f4300f7b338ada7cab868a7998007b29ec5203c
Charlie Yi Zhu <charliezhu@Charlies-MacBook-Pro.local>
2018-08-29 12:19:09 -0700

    closest K Values in BST

--- coding_algorithm/jiu_zhang/lintcode.py
+++ coding_algorithm/jiu_zhang/lintcode.py
@@ -1050,21 +1050,108 @@ class BSTIterator:
     #@param root: The root of binary tree.
     def __init__(self, root):
         self.stack = []
-        self.current_node = root
+        self.pointer_node = root
 
     #@return: True if there has next node, or false
     def hasNext(self):
-        return self.current_node is not None or len(self.stack) > 0
+        return self.pointer_node is not None or len(self.stack) > 0
 
     #@return: return next node
     def next(self):
-        while self.current_node is not None:
-            self.stack.append(self.current_node)
-            self.current_node = self.current_node.left
+        while self.pointer_node is not None:
+            self.stack.append(self.pointer_node)
+            self.pointer_node = self.pointer_node.left
             
-        self.current_node = self.stack.pop()
-        visit_node = self.current_node
-        self.current_node = self.current_node.right
+        self.pointer_node = self.stack.pop()
+        visit_node = self.pointer_node
+        self.pointer_node = self.pointer_node.right
         return visit_node
+#
+
+failed test case, but Submit was accepted.
+{1, #, 2, #, 3}
+1.1
+1
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+class Solution:
+    """
+    @param root: the given BST
+    @param target: the given target
+    @param k: the given k
+    @return: k values in the BST that are closest to the target
+    """
+    def closestKValues(self, root, target, k):
+        if root is None:
+            return []
+        stack_upper, stack_lower, A = [], [], []
+        self.find_target(root, target, stack_upper)
+        stack_lower = stack_upper[:]
+        if stack_upper < target:
+            self.move_upper()
+        else:
+            self.move_lower()
+        for _ in range(k):
+            if not stack_upper:
+                A.append(stack_lower[-1])
+                self.move_lower
+            elif not stack_lower:
+                A.append(stack_upper[-1])
+                self.move_upper()
+            elif stack_upper[-1].val - target < target - stack_lower[-1].val:
+                A.append(stack_upper[-1])
+                self.move_upper()
+            else:
+                A.append(stack_lower[-1])
+                self.move_lower()
+    def find_target(self, root, target, stack):
+        while root:
+            stack.append(root)
+            if target < root.val:
+                root = root.left
+            else:
+                root = root.right
+    def move_upper(self, stack):
+        curr = stack[-1]
+        if curr.right:
+            node = curr.right
+            while node:
+                stack.append(node)
+                node = node.left
+        else:
+            visited_node = stack.pop() 
+            while stack and stack[-1].right = visited_node:
+                visited_node = stack.pop()
+    def move_lower(self, stack):
+        curr = stack[-1]
+        if curr.left:
+            node = curr.left
+            while node:
+                stack.append(node)
+                node = node.right
+        else:
+            visited_node = stack.pop() 
+            while stack and stack[-1].left = visited_node:
+                visited_node = stack.pop()
+
 
 #
+
+bst = TreeNode(5)
+bst.left = TreeNode(3)
+bst.left.left = TreeNode(1)
+bst.right = TreeNode(8)
+bst.right.left = TreeNode(7)
+bst.right.right = TreeNode(10)
+
+test = Solution()
+test.closestKValues(bst,4,6)
+
+bst = TreeNode(1)
+test = Solution()
+test.closestKValues(bst,0.0,1)
